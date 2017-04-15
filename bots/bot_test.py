7271f780e7b05cb7a748634a8ab92d68671c9299
Andrey Klimenko <klem4@MacBook-Pro-Andrey.local>
2017-04-15 16:25:09 +0300

    timple bot in progress

--- bots/bot_test.py
+++ bots/bot_test.py
@@ -1,6 +1,8 @@
 # coding: utf-8
+from aiohttp.http_websocket import WSMessage
 from bots.prototype import SlackBotPrototype
 
 
 class TestBot(SlackBotPrototype):
-    pass
+    async def process_message(self, msg: WSMessage):
+        self.logger.info("RECEIVE MESSAGE: [%s] %s" % (msg.tp, msg.data))
--- bots/prototype.py
+++ bots/prototype.py
@@ -1,13 +1,34 @@
 # coding: utf-8
 
 import asyncio
+import aiohttp
+from aiohttp.http_websocket import WSMessage
+from log import default_logger
 from slack.client import SlackAsyncClient
 
 
 class SlackBotPrototype:
-    def __init__(self, loop: asyncio.BaseEventLoop, client: SlackAsyncClient):
+    def __init__(self, loop: asyncio.BaseEventLoop, client: SlackAsyncClient,
+                 logger=default_logger, loop_wait=1):
         self.client = client
         self.loop = loop
+        self.logger = logger
+        self.loop_wait = loop_wait
 
     async def run(self):
         await self.client.start()
+        session = aiohttp.ClientSession()
+
+        ws = await session.ws_connect(self.client.wss_url)
+
+        while True:
+            self.logger.debug("loop iteration")
+            try:
+                msg = await ws.receive()
+                await self.process_message(msg)
+                await asyncio.sleep(self.loop_wait)
+            except Exception as e:
+                self.logger.exception("Unhandled exception")
+
+    async def process_message(self, msg: WSMessage):
+        raise NotImplementedError()
